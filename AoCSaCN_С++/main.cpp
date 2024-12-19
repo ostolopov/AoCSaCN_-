1951281883433ce966ae5aecb3a5954d20114efe
ostolopov <egor21231bar@gmail.com>
2024-12-20 00:24:44 +0300

    Initial Commit

---
+++ AoCSaCN_С++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AoCSaCN_С++
+//
+//  Created by Egor Baranov on 20.12.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
